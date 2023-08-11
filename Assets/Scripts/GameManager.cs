7f2a16632fdd618fd53322b26c0693ea486a2fcf
Efe Polat <mefepolat@hotmail.com>
2023-08-11 13:09:15 -0400

    implemented Main Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour
     {
         if (_isGameOver && Input.GetKeyDown(KeyCode.R))
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(1);
         }
     }
     public void GameOver()
